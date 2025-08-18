d70003c3e4d1dff41e122da6d8d89512d324ddfc
Moeed Sultan <moeedkundi@gmail.com>
2025-08-18 22:05:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def load_model():
     
     genai.configure(api_key=api_key)
     return genai.GenerativeModel(
-        "gemini-2.0-flash-exp",
+        "gemini-2.5-flash",
         generation_config=GenerationConfig(temperature=0.3)
     )
 
